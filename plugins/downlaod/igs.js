725e26900ad8d16c07203e72af724a6617057d2c
Jennie...! <108253179+Jenniexcute@users.noreply.github.com>
2023-01-11 00:39:44 -1200

    Update igs.js

--- plugins/downlaod/igs.js
+++ plugins/downlaod/igs.js
@@ -11,7 +11,7 @@ exports.run = {
    }) => {
       try {
          if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'hosico_cat'), m)
-         client.sendReact(m.chat, '🕒', m.key)
+         client.sendReact(m.chat, '✨', m.key)
          let old = new Date()
          let json = await Api.igs(args[0])
          if (!json.status) return client.reply(m.chat, global.status.fail, m)
